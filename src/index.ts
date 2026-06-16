436e40700ea8d9b2644f84b1264c9341e33a018e
andretakeo <andrefujiwara1@gmail.com>
2026-06-16 17:12:02 -0300

    implement health check route and start server with Bun

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log("Hello via Bun!");
+const server = Bun.serve({
+  routes: {
+    "/health": {
+      GET: () =>
+        Response.json({
+          status: "ok",
+        }),
+    },
+  },
+});
+
+console.log(`Server running at ${server.url}`);
