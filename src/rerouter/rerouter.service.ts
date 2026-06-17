45550ceaadb6c0155e32d97abcd88b653e6cab31
andretakeo <andrefujiwara1@gmail.com>
2026-06-17 11:25:15 -0300

    feat(rerouter): update reroute function to return 404 for missing URLs and redirect for found URLs

--- src/rerouter/rerouter.service.ts
+++ src/rerouter/rerouter.service.ts
@@ -3,8 +3,9 @@ import { redis } from "bun";
 export const rerouteFromShortUrl = async ({ urlHash }: { urlHash: string }) => {
   const originalUrl = await redis.get(`url:${urlHash}`);
 
-  return Response.json({
-    message: "Success",
-    originalUrl,
-  });
+  if (!originalUrl) {
+    return Response.json({ message: "URL not found" }, { status: 404 });
+  }
+
+  return Response.redirect(originalUrl, 301);
 };
